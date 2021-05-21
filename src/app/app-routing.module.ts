cbcf228aa3c7feb00c9b804576973acd848a5763
Luan Chuong Nguyen <lcnguyen@codix.eu>
2021-05-21 16:48:47 +0700

    im luan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DemoModule } from './pages/demo/demo.module';
 
 const routes: Routes = [
   {
